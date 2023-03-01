5c2d6b7d38491ee5aae5793736e205d8e2a35b85
Nastya <P19042009@yandex.ru>
2023-03-01 07:42:23 +0300

    add pokemon in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
+'use strict';
 window.onload = function () {
     if (JSON.parse(localStorage.getItem('userdata')) == null) {
         window.location.href = 'registration.html'
     } else { document.getElementById("username_header").textContent = JSON.parse(localStorage.getItem('userdata'))['username'] }
 };
-
-'use strict';
+//показ покемонов из хранилища
+document.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('htmlinner') !== null) {
+    const htmlarr = JSON.parse(localStorage.getItem('htmlinner'));
+    let str = htmlarr.join('');
+    tableAppendChild.innerHTML = str;
+    }
+    })
 //добавление покемонов
 const btn = document.querySelector('.button');
 const pokemonUser = document.getElementById('pokemonName');
+const tableAppendChild = document.getElementById('tableAppendChild');
 let pokemonMassive = [];
+let htmlinner = [];
 btn.addEventListener('click', function () {
     const pokemon = pokemonUser.value.toLowerCase();
-    const tableAppendChild = document.getElementById('tableAppendChild');
     if (!pokemonUser.value) {
         alert('Поле пустое')
         return
@@ -19,6 +27,12 @@ btn.addEventListener('click', function () {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
             .then((response) => response.json())
             .then((data) => {
+                //сохранение в хранилище покемонов с данными
+                const pokemonUserSave = {pokemonName: pokemon, height: data.height, weight: data.weight, ability:
+                data.abilities[0].ability.name, photo: data.sprites.front_default};
+                pokemonMassive.push(pokemonUserSave);
+                localStorage.setItem('pokemonUserSave', JSON.stringify(pokemonMassive));
+                //вывод строки
                 const createElTr = document.createElement('tr');
                 createElTr.classList.add('grey_tr');
                 tableAppendChild.appendChild(createElTr);
@@ -49,13 +63,12 @@ btn.addEventListener('click', function () {
                 createTdPic.appendChild(createPic);
                 createTdPic.classList.add('pokemon_pic');
                 createElTr.appendChild(createTdPic);
-                //сохранение в хранилище покемонов с данными
-                const pokemonUserSave = { pokemonName: pokemon, height: data.height, weight: data.weight, ability: data.abilities[0].ability.name, photo: data.sprites.front_default };
-                pokemonMassive.push(pokemonUserSave);
-                for (let i = 0; i < pokemonMassive.length; i++) {
-                    localStorage.setItem(`${pokemon}`, JSON.stringify(pokemonMassive[i]))
-                }
-            })
+                //сохранение кода в хранилище
+                htmlinner = [];
+                localStorage.removeItem('htmlinner');
+                htmlinner.push(tableAppendChild.innerHTML);
+                localStorage.setItem('htmlinner', JSON.stringify(htmlinner));
+                })
             .catch((err) => {
                 alert('Покемон не найден или неправильно набрано имя');
                 console.log(err);
@@ -67,6 +80,7 @@ function clearStorage() {
     if (localStorage.getItem('userdata') !== null) {
         localStorage.removeItem('userdata');
         localStorage.removeItem(`pokemonUserSave`);
+        localStorage.removeItem(`htmlinner`);
         location.href = location.href;
     }
 }
